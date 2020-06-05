f4a8305c9ca47cfc044351d859d3030bbdd1808c
Shagun Sodhani <sodhani@fb.com>
2020-06-05 03:31:46 -0700

    Separate tests for lint and mypy

--- noxfile.py
+++ noxfile.py
@@ -7,5 +7,14 @@ def lint(session):
     session.install("--upgrade", "setuptools", "pip")
     session.install("-r", "requirements.txt")
     session.run("flake8", "codes")
+    session.run("flake8", "main.py")
     session.run("black", "--check", "codes")
+    session.run("black", "--check", "main.py")
+
+
+@nox.session(venv_backend="conda")
+def mypy(session):
+    session.install("--upgrade", "setuptools", "pip")
+    session.install("-r", "requirements.txt")
     session.run("mypy", "--strict", "codes")
+    session.run("mypy", "--strict", "main.py")
